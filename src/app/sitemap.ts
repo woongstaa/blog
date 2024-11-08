886d9bc2d9f7ed7a933a9311fd7d62583b954053
이진웅 <jinung91@gmail.com>
2024-11-08 18:03:48 +0900

    feat: sitemap 추가

---
+++ src/app/sitemap.ts
@@ -0,0 +1,37 @@
+import { PostsImpl } from '@/entities';
+import type { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const BASE_URL = 'https://jaylog.dev';
+  const posts = new PostsImpl().create();
+
+  const staticSitemap: MetadataRoute.Sitemap = [
+    {
+      url: BASE_URL,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5
+    },
+    {
+      url: `${BASE_URL}/posts`,
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.8
+    },
+    {
+      url: `${BASE_URL}/portfolio`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1
+    }
+  ];
+
+  const postsSitemap: MetadataRoute.Sitemap = posts.map((post) => ({
+    url: `${BASE_URL}/posts/${post.category}/${post.id}`,
+    lastModified: new Date(),
+    changeFrequency: 'daily',
+    priority: 1
+  }));
+
+  return [...staticSitemap, ...postsSitemap];
+}
