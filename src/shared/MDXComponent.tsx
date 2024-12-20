d47c703a294df49f848c0e97725c533a31263cfe
이진웅 <jinung91@gmail.com>
2024-12-20 16:39:49 +0900

    chore: rehypePrettyCode 테스트 끝

--- src/shared/MDXComponent.tsx
+++ src/shared/MDXComponent.tsx
@@ -1,31 +1,43 @@
 import { MDXRemote } from 'next-mdx-remote/rsc';
-// import rehypePrettyCode from 'rehype-pretty-code';
+import rehypePrettyCode from 'rehype-pretty-code';
+import rehypeSlug from 'rehype-slug';
 import remarkBreaks from 'remark-breaks';
 import remarkGfm from 'remark-gfm';
 // import { transformerCopyButton } from '@rehype-pretty/transformers';
+import remarkToc from 'remark-toc';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 
-export function MDXComponent({ content }: { content: string }) {
+export function MDXComponent({ content }: { content?: string }) {
   return (
     <article className='prose mx-auto w-full'>
       <MDXRemote
-        source={content}
+        source={content || ''}
         options={{
           mdxOptions: {
-            remarkPlugins: [remarkGfm, remarkBreaks],
+            remarkPlugins: [remarkGfm, remarkBreaks, [remarkToc, { heading: 'structure' }]],
             rehypePlugins: [
-              // [
-              //   rehypePrettyCode,
-              //   {
-              //     keepBackground: false,
-              //     theme: { dark: 'plastic', light: 'github-light' }
-              //     // transformers: [
-              //     //   transformerCopyButton({
-              //     //     visibility: 'always',
-              //     //     feedbackDuration: 3_000
-              //     //   })
-              //     // ]
-              //   }
-              // ]
+              [
+                rehypePrettyCode,
+                {
+                  keepBackground: false,
+                  theme: { dark: 'plastic', light: 'github-light' }
+                  // transformers: [
+                  //   transformerCopyButton({
+                  //     visibility: 'always',
+                  //     feedbackDuration: 3_000
+                  //   })
+                  // ]
+                }
+              ],
+              rehypeSlug,
+              [
+                rehypeAutolinkHeadings,
+                {
+                  properties: {
+                    className: ['anchor']
+                  }
+                }
+              ]
             ]
           }
         }}
